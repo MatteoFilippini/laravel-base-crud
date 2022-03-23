a6e76cb1385eeb76b5932da688bfadee2943d946
MatteoFilippini <matteofilippini8@gmail.com>
2022-03-23 14:49:35 +0100

    DELETE comic

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -6,4 +6,10 @@
 <p>{{ $comic->price }}</p>
 <a href="{{ route('comics.edit',$comic->id) }}">modifica</a>
 <a href="{{ route('comics.index') }}">Tutti i comics</a>
+<br>
+<form action="{{ route('comics.destroy',$comic->id) }}" method="POST">
+    @csrf
+    @method('DELETE')
+    <button type="submit">elimina</button>
+</form>
 @endsection
